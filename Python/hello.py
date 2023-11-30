0e6b7ffde6bf3200ec7b4e00c730ff293e2ece26
JiteshSing <singhjitesh575@gmail.com>
2023-11-30 14:13:50 +0530

    done

---
+++ Python/hello.py
@@ -0,0 +1,37 @@
+import datetime
+
+'''x = datetime.datetime.now()
+y = datetime.datetime(2018, 6, 1)
+print(y)'''
+
+
+def sum(a,b):
+    print("Sum = ",a+b)
+def subtraction(a,b):
+    print("Subtraction = ",a-b)
+
+def multiply(a,b):
+    print("Multiply = ",a*b)
+
+def divide(a,b):
+    print("Divide = ",a/b)
+
+def modulas(a,b):
+    print("Modulas = ", a%b)
+def percentage(a,b):
+     c = ((a*100)/b)
+     print("Percentage = ",c)
+
+sum(52,8)
+sum(45,45)
+sum(100,25)
+subtraction(100,125)
+subtraction(45,40)
+multiply(2,3)
+multiply(10,90)
+divide(20,10)
+divide(30,3)
+modulas(30,4)
+modulas(22,3)
+percentage(307,500)
+percentage(400,500)
---
+++ Python/ifelse.py
@@ -0,0 +1,119 @@
+#Dt- 27/11/23
+# if condition
+price = 110
+money = 99
+'''if( money > price ):
+    print("I can not buy pizza")
+    print("Feeling bad :(")
+else:
+    print("I can buy pizza")
+    print("Yum Yum :)")
+    '''
+'''# Print even and odd
+num1 = int(input("Enter a number"))
+if( num1 % 2 == 0):
+    print("It's a even Number")
+else:
+    print("It's a odd number")
+'''
+'''# Print check alphabet
+char = input("Enter a Alphabet ")
+if('a'<= char <= 'z'):
+    print("Enter char is alphabet")
+else:
+    print("Enter char is not alphabet")
+'''
+# Print number
+'''char = (input("Enter a character "))
+
+if( '0' <= char <= '9'):
+    print("Enter a char is number")
+else:
+    print("Enter char is not number")
+'''
+# Print number divisible by 3:
+'''
+num1 = int(input("Enter a  number "))
+if( num1%3 == 0 ):
+    print(num1, " is divisible by 3")
+else:
+    print(num1, "is not divisible by 3")'''
+# Print num divisible by 5 & 7 :
+'''num1 = int(input("Enter a num "))
+if(num1 % 5 == 0 and num1 % 7 ==0):
+    print(num1, "is a divisible by both 5 & 7")
+else:
+    print(num1, "is not divisible by 5 & 7")'''
+# Write a Program to check whether a person is eligible for voting or not.(accept any age from user)
+'''Age = int(input("Enter a age of Person "))
+if( Age  >= 18 ):
+    print("You're eligible for voting ")
+else:
+    print("You're not eligible for voting")'''
+# Write a Program to check whether number is positive or negative
+'''num1 = int(input("Enter a number "))
+if( num1 > 0):
+    print(num1, "is positive number")
+else:
+    print(num1, "is negative number")'''
+
+# Write a program to find a largest number out of 3 numbers excepted from user
+'''num1 = int(input("Enter a num1 "))
+num2 = int(input("Enter a num2 "))
+num3 = int(input("Enter a num3 "))
+
+if(num1> num2 and num1>num3):
+    print(num1, "is greater number")
+if(num2 > num1 and num2 > num3):
+    print(num2, "is greatest number")
+if(num3 > num1 and num3 > num2):
+    print(num3, "is greatest number")
+else:
+    print("Don't enter same number")'''
+
+# Write a program to find a lowest number out of 2 numbers excepted from user
+'''num1 = int(input("Enter a num1 "))
+num2 = int(input("Enter a num2 "))
+if(num1> num2):
+    print(num2, "is lowest number")
+else:
+    print(num1, "is lowest number")'''
+# Write accept the temperature in degree celsius of water and check whether it is boiling or not
+'''temp = int(input("Enter a temperature "))
+if(temp >= 100):
+    print("It's boiling temperature")
+else:
+    print("It's not a boiling temperature")'''
+#  Write a program to display "Hello" if a number entered by user is multiplied by 5 otherwise print "bye"
+'''num1 = int(input("Enter a number "))
+if(num1%5==0):
+    print("Hello")
+else:
+    print("Bye")'''
+#  Write a program to accept percentage from the user and display the grade according to the following criteria:
+'''Mark                Grades
+>90                      A
+>80 and <=90             B
+>=60 and <=80            C
+below 60                 D
+'''
+'''per = int(input("Enter a Percentage "))
+if(per>90):
+    print("You passed in A grade")
+if(per > 80 and per <= 90):
+    print("You passed in B grade")
+if (per>=60 and per<=80):
+    print("You passed in C grade")
+if(per<60):
+    print("You passed in D grade")'''
+
+
+# Take values of length and breadth of a rectangle from user and check if it is square or not.
+'''length = int(input("Enter length "))
+breadth = int(input("Enter breadth "))
+if(length == breadth ):
+    print("It's a square.")
+else:
+    print("It's a rectangle")'''
+
+
