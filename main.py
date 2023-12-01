bc71846318a1d9f64d5cbca28e9d321f96e19d75
JiteshSing <singhjitesh575@gmail.com>
2023-12-01 12:08:32 +0530

    done

---
+++ main.py
@@ -0,0 +1,17 @@
+# Assign value with help of = operator
+a, b, c = 10, 20, "Sunil OS"
+print(a)
+print(b)
+print(c)
+
+# print same variable in two different value
+name = "Rays"
+
+name = "Computer"
+print(name)
+# print two num and add them
+num1 = int(input("Enter 1st num"))
+num2 = int(input("Enter 2nd num"))
+sum = num1 / num2
+print("Sum of two num = " , sum)
+
---
+++ whileloop.py
@@ -0,0 +1,67 @@
+# Print value of 1 to 10;
+'''i=0
+while(i<=10):
+    print("num = ",i)
+    i +=1'''
+# Print value of 1 to 100:
+'''i=0
+while(i<=100):
+    print("num = ",i)
+    i +=1'''
+#Print even number upto 20:
+'''i =1
+while(i<=20):
+    if(i%2==0):
+        print("Even no. ",i)
+    i+=1
+'''
+# Print only number which are divisible by 4 & 5 upto 100
+'''i=1
+while(i<=100):
+    if(i%4==0 and i%5==0):
+        print("Num = ",i)
+    i+=1'''
+# Print table of 2:
+'''i=1
+while(i<=10):
+    j=i*2
+    print("2 * ",i," = ",j)
+    i+=1
+'''
+# print table of 5:
+'''i=1
+while(i<=10):
+    j=i*5
+    print("5 * ",i," = ",j)
+    i+=1'''
+# print divisible of 3 & 5 upto 100:
+'''i=1
+while(i<=100):
+ if(i%3==0 and i%5==0):
+   print("Num divide by 3 & 5 = ",i)
+ i +=1
+'''
+#print odd number upto 100:
+'''i=1
+while(i<=100):
+    if(i%2==1):
+        print("Odd no. = ",i)
+    i+=1'''
+# print table of 13 upto 8:
+'''i=1
+while(i<=10):
+    j= i*13
+    if(i == 8):
+        continue
+    print("13 * ",i," = ",j)
+    i+=1'''
+#print table of 12 upto 10 but not print 8:
+'''i=1
+while(i<=10):
+    j=i*12
+    if(i==7):
+        continue
+    print("12 * ",i," = ",j)
+    i +=1
+    '''
+
